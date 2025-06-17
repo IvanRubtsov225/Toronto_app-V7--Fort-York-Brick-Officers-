19db26687c8f61cf8c74d9954b231ac240d7b3f7
Ivan Rubtsov <Ivanrubtsovru@gmail.com>
2025-06-17 19:56:38 -0400

    Back rework
    Separate copy made

--- API + WEB page/src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Toronto Hidden Gems Finder - Source Package
-
-This package contains all the core modules for finding hidden gems in Toronto
-by combining Reddit data with DineSafe inspection records.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Toronto Hidden Gems Team" 
